e0371ecebffd7af36b785b75d85ee64bfe245769
jazzjohon <samueljazzjohn@gmail.com>
2024-05-30 20:10:48 +0530

    source data added

--- app/config/constants.py
+++ app/config/constants.py
@@ -4,4 +4,5 @@ TRAINED_DATA_URL= getenv('TRAINED_DATA_URL', '')
 HOST= getenv('HOST', '')
 SCHEME= getenv('SCHEME', '')
 TRAINED_FILE_NAME="ara.traineddata"
-UPLOAD_DIR="app/config/data"
+UPLOAD_DIR="app/config/data/extracted_data"
+SOURCE_DIR="app/config/data/source_data"
